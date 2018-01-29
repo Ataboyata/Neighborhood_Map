3f400b1a329180121ba567b60e4377134169b39a
Ataboyata <33212339+Ataboyata@users.noreply.github.com>
2018-01-29 11:00:25 -0600

    Added JQuery and Knockout

--- js/script.js
+++ js/script.js
@@ -380,14 +380,42 @@ map = new google.maps.Map(document.getElementById('map'), {
 
 // These are 7 of the historical locations in Queretaro
 var locations = [
-	{title: 'Los Arcos Queretaro', location: {lat: 20.596527, lng: -100.372172}},
-  	{title: 'Panteón de los Queretanos Ilustres', location: {lat: 20.592484, lng: -100.380225}},
-  	{title: 'Parque Queretaro 2000', location: {lat: 20.618610, lng: -100.394924}},
-	{title: 'Plaza de Toros Santa Maria', location: {lat: 20.574777, lng: -100.405222}},
-	{title: 'Cerro de las Campanas', location: {lat: 20.593326, lng: -100.410203}},
-	{title: 'Plaza de Armas', location: {lat: 20.592939, lng: -100.389649}},
-	{title: 'Jardin Zenea', location: {lat: 20.592761, lng: -100.392123}}
-];
+	{
+        title: 'Los Arcos Queretaro',
+        location: {lat: 20.596527, lng: -100.372172},
+        id: "loc1",
+    },
+  	{
+        title: 'Panteón de los Queretanos Ilustres',
+        location: {lat: 20.592484, lng: -100.380225},
+        id: "loc2",
+    },
+  	{
+        title: 'Parque Queretaro 2000',
+        location: {lat: 20.618610, lng: -100.394924},
+        id: "loc3",
+    },
+	{
+        title: 'Plaza de Toros Santa Maria',
+        location: {lat: 20.574777, lng: -100.405222},
+        id: "loc4",
+    },
+	{
+        title: 'Cerro de las Campanas',
+        location: {lat: 20.593326, lng: -100.410203},
+        id: "loc5",
+    },
+	{
+        title: 'Plaza de Armas',
+        location: {lat: 20.592939, lng: -100.389649},
+        id: "loc6",
+    },
+	{
+        title: 'Jardin Zenea',
+        location: {lat: 20.592761, lng: -100.392123},
+        id: "loc7",
+    }
+    ];
 
 var largeInfowindow = new google.maps.InfoWindow();
 
