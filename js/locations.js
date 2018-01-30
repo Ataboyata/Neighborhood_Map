3483f8c085575a728862cf452d92b8f00d6e3673
Ataboyata <33212339+Ataboyata@users.noreply.github.com>
2018-01-30 12:29:04 -0600

    Separated the Locations and the Map Styles to Other Files

---
+++ js/locations.js
@@ -0,0 +1,38 @@
+// These are 7 of the historical locations in Queretaro
+var locations = [
+    {
+        title: 'Los Arcos Queretaro',
+        location: {lat: 20.596527, lng: -100.372172},
+        id: "loc1",
+    },
+    {
+        title: 'Panteón de los Queretanos Ilustres',
+        location: {lat: 20.592484, lng: -100.380225},
+        id: "loc2",
+    },
+    {
+        title: 'Parque Queretaro 2000',
+        location: {lat: 20.618610, lng: -100.394924},
+        id: "loc3",
+    },
+    {
+        title: 'Plaza de Toros Santa Maria',
+        location: {lat: 20.574777, lng: -100.405222},
+        id: "loc4",
+    },
+    {
+        title: 'Cerro de las Campanas',
+        location: {lat: 20.593326, lng: -100.410203},
+        id: "loc5",
+    },
+    {
+        title: 'Plaza de Armas',
+        location: {lat: 20.592939, lng: -100.389649},
+        id: "loc6",
+    },
+    {
+        title: 'Jardin Zenea',
+        location: {lat: 20.592761, lng: -100.392123},
+        id: "loc7",
+    }
+];
