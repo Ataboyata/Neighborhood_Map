bc61a0f37a2dfc3c1564d7e3076d6c5ef6302973
Ataboyata <33212339+Ataboyata@users.noreply.github.com>
2018-01-30 13:17:17 -0600

    Debugging and Changes inthe JS callback functions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
 // Global Variables
 var map
 
-var ViewModel = function(){
+function ViewModel() {
     var self = this;
 
     // Create a new blank array for all the listing markers.
     var markers = [];
     
-    this.initMap() = function() {
+    this.initMap = function() {
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
             center: new google.maps.LatLng(20.591676, -100.380815),
             zoom: 13,
             styles: styles
         };
+        // Creates Map
         map = new google.maps.Map(mapCanvas, mapOptions);
+
+        // Set InfoWindow
+        this.largeInfoWindow = new google.maps.InfoWindow();
+        for (var i = 0; i < myLocations.length; i++) {
+            this.markerTitle = myLocations[i].title;
+            this.markerLat = myLocations[i].lat;
+            this.markerLng = myLocations[i].lng;
+            // Google Maps marker setup
+            this.marker = new google.maps.Marker({
+                map: map,
+                position: {
+                    lat: this.markerLat,
+                    lng: this.markerLng
+                },
+                title: this.markerTitle,
+                lat: this.markerLat,
+                lng: this.markerLng,
+                id: i,
+                animation: google.maps.Animation.DROP
+            });
+            this.marker.setMap(map);
+            this.markers.push(this.marker);
+            this.marker.addListener('click', self.populateAndBounceMarker);
+        }
     };
     this.initMap();
-};
+}
 
 googleError = function googleError() {
     alert('Google Maps did not load, please refresh the page');
