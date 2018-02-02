288440a15d4adc3c0b318d6c51b0205ed7888974
Ataboyata <33212339+Ataboyata@users.noreply.github.com>
2018-02-02 11:44:12 -0600

    Beautified

--- js/markers.js
+++ js/markers.js
@@ -1,53 +1,82 @@
 // These are 7 of the historical locations in Queretaro
-var myLocations = [
-    {
+var myLocations = [{
         title: 'Acueducto de Querétaro',
-        location: {lat: 20.596527, lng: -100.372172},
+        location: {
+            lat: 20.596527,
+            lng: -100.372172
+        },
         id: "loc1",
     },
     {
         title: 'Panteón de los Queretanos Ilustres',
-        location: {lat: 20.592484, lng: -100.380225},
+        location: {
+            lat: 20.592484,
+            lng: -100.380225
+        },
         id: "loc2",
     },
     {
         title: 'El Cerrito (Querétaro)',
-        location: {lat: 20.551246, lng: -100.443444},
+        location: {
+            lat: 20.551246,
+            lng: -100.443444
+        },
         id: "loc3",
     },
     {
         title: 'Estadio Corregidora',
-        location: {lat: 20.578008, lng: -100.366087},
+        location: {
+            lat: 20.578008,
+            lng: -100.366087
+        },
         id: "loc4",
     },
     {
         title: 'Cerro de las Campanas',
-        location: {lat: 20.593326, lng: -100.410203},
+        location: {
+            lat: 20.593326,
+            lng: -100.410203
+        },
         id: "loc5",
     },
     {
         title: 'Centro histórico de Santiago de Querétaro',
-        location: {lat: 20.592939, lng: -100.389649},
+        location: {
+            lat: 20.592939,
+            lng: -100.389649
+        },
         id: "loc6",
     },
     {
         title: 'Templo y exconvento de San Francisco de Asís de Querétaro',
-        location: {lat: 20.592761, lng: -100.392123},
+        location: {
+            lat: 20.592761,
+            lng: -100.392123
+        },
         id: "loc7",
     },
     {
         title: 'Monterrey Institute of Technology and Higher Education',
-        location: {lat: 20.613333, lng: -100.403586},
+        location: {
+            lat: 20.613333,
+            lng: -100.403586
+        },
         id: "loc8",
     },
     {
         title: 'Parque nacional El Cimatario',
-        location: {lat: 20.546425, lng: -100.350564},
+        location: {
+            lat: 20.546425,
+            lng: -100.350564
+        },
         id: "loc9",
     },
     {
         title: 'Universidad Autónoma de Querétaro',
-        location: {lat: 20.591309, lng: -100.410866},
+        location: {
+            lat: 20.591309,
+            lng: -100.410866
+        },
         id: "loc10",
     }
 ];
